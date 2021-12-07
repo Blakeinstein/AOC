f2f09e10ffb621480970f5ecefa342995d196f6d
Blaine <rishi1998@gmail.com>
2021-12-07 15:39:38 +0530

    More optimal sol

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -7,25 +7,19 @@ fn get_input(input: &str) -> Vec<i32> {
 }
 
 pub fn part1(input: String) {
-  let input = get_input(&input);
-  let mut part1 = i32::MAX;
-  for pos in input.iter() {
-    let curr_sum = input.iter().map(|x| (x - pos).abs()).sum::<i32>();
-    part1 = part1.min(curr_sum);
-  }
+  let mut input = get_input(&input);
+  input.sort_unstable();
+  let opt_pos = input[input.len() / 2];
+  let part1 = input.iter().map(|x| (x - opt_pos).abs()).sum::<i32>();
   dbg!(part1);
 }
 
 pub fn part2(input: String) {
   let input = get_input(&input);
-  let mut part2 = i32::MAX;
-  let max_pos = input.iter().max().unwrap();
-  for pos in 0..=*max_pos {
-    let curr_sum = input.iter().map(|x| {
-      let n = (x - pos).abs();
-      n * (n + 1) / 2
-    }).sum::<i32>();
-    part2 = part2.min(curr_sum);
-  }
+  let opt_pos = input.iter().sum::<i32>() / input.len() as i32;
+  let part2 = input.iter().map(|x| {
+    let n = (x - opt_pos).abs();
+    n * (n + 1) / 2
+  }).sum::<i32>();
   dbg!(part2);
 }
