cf65e7fa73b71bcdfded970acfad6c407bc6f50a
Blaine <rishi1998@gmail.com>
2021-12-07 12:24:46 +0530

    Less goooo

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -1,7 +1,31 @@
+fn get_input(input: &str) -> Vec<i32> {
+  input
+    .lines().next().unwrap()
+    .split(',')
+    .map(|x| x.parse::<i32>().unwrap())
+    .collect()
+}
+
 pub fn part1(input: String) {
-  todo!();
+  let input = get_input(&input);
+  let mut part1 = i32::MAX;
+  for pos in input.iter() {
+    let curr_sum = input.iter().map(|x| (x - pos).abs()).sum::<i32>();
+    part1 = part1.min(curr_sum);
+  }
+  dbg!(part1);
 }
 
 pub fn part2(input: String) {
-  todo!();
+  let input = get_input(&input);
+  let mut part2 = i32::MAX;
+  let max_pos = input.iter().max().unwrap();
+  for pos in 0..=*max_pos {
+    let curr_sum = input.iter().map(|x| {
+      let n = (x - pos).abs();
+      n * (n + 1) / 2
+    }).sum::<i32>();
+    part2 = part2.min(curr_sum);
+  }
+  dbg!(part2);
 }
