3c398ec6366e8e6f67ed47c4b7a37cfe0222044a
Blaine <rishi1998@gmail.com>
2021-12-11 13:38:24 +0530

    day 11 solved

--- 2021/src/day11.rs
+++ 2021/src/day11.rs
@@ -1,7 +1,71 @@
+use std::collections::HashSet;
+use std::convert::TryInto;
+
+fn get_input(input: &str) -> [[u32; 10]; 10] {
+  input
+    .lines()
+    .map(|line| {
+      line
+        .chars()
+        .map(|x| x.to_digit(10).unwrap())
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap()
+    })
+    .collect::<Vec<_>>()
+    .try_into()
+    .unwrap()
+}
+
+fn flash(map: &mut [[u32; 10]; 10], i: usize, j: usize, blink_set: &mut HashSet<(usize, usize)>) {
+  if i > 9 ||
+    j > 9 ||
+    blink_set.contains(&(i, j)) {
+    return
+  }
+  map[i][j] += 1;
+  if map[i][j] > 9 {
+    blink_set.insert((i, j));
+    map[i][j] = 0;
+    flash(map, i + 1, j, blink_set); // ->
+    flash(map, i + 1, j + 1, blink_set); // ->^
+    flash(map, i, j + 1, blink_set); // ^
+    flash(map, i.wrapping_sub(1), j + 1, blink_set); // <-^
+    flash(map, i.wrapping_sub(1), j, blink_set); // <-
+    flash(map, i.wrapping_sub(1), j.wrapping_sub(1), blink_set); // <-v
+    flash(map, i, j.wrapping_sub(1), blink_set); // v
+    flash(map, i + 1, j.wrapping_sub(1), blink_set); // ->v
+  }
+}
+
 pub fn part1(input: String) {
-  todo!();
+  let mut input = get_input(&input);
+  let mut part1 = 0;
+  let mut blink_set = HashSet::new();
+  for _ in 0..100 {
+    for i in 0..10 {
+      for j in 0..10 {
+        flash(&mut input, i, j, &mut blink_set);
+      }
+    }
+    part1 += blink_set.len();
+    blink_set.clear();
+  }
+  dbg!(part1);
 }
 
 pub fn part2(input: String) {
-  todo!();
+  let mut input = get_input(&input);
+  let mut part2= 0;
+  let mut blink_set = HashSet::new();
+  while blink_set.len() < 10 * 10 {
+    blink_set.clear();
+    for i in 0..10 {
+      for j in 0..10 {
+        flash(&mut input, i, j, &mut blink_set);
+      }
+    }
+    part2 += 1;
+  }
+  dbg!(part2);
 }
