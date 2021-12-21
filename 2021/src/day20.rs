7b6e1842d718357a96063ebf1c21db81c4f8fda5
Blaine <rishi1998@gmail.com>
2021-12-21 15:54:10 +0530

    Day 21 solved

--- 2021/src/day20.rs
+++ 2021/src/day20.rs
@@ -1,7 +1,97 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Copy, Debug)]
+enum Pixel {
+  Dark = 0,
+  Light = 1,
+}
+
+impl Pixel {
+  fn from_char(c: char) -> Option<Self> {
+    match c {
+      '#' => Some(Pixel::Light),
+      '.' => Some(Pixel::Dark),
+      _ => None
+    }
+  }
+}
+
+type ImageLookup = HashMap<(i32, i32), Pixel>;
+
+struct Image {
+  lookup: ImageLookup,
+  offset: i32,
+  dim: (usize, usize)
+}
+
+struct Puzzle {
+  algo: Vec<Pixel>,
+  image: Image
+}
+
+fn get_input(input: &str) -> Puzzle {
+  let mut lines = input.lines();
+  let algo = lines.next().unwrap().chars().map(|point| Pixel::from_char(point).unwrap()).collect();
+  let mut dim = (0, 0);
+  let lookup = lines.skip(1).enumerate()
+  .flat_map(|(i, line)|
+    line
+    .chars()
+    .enumerate()
+    .map(|(j, c)| {
+      dim.0 = dim.0.max(i);
+      dim.1 = dim.1.max(j);
+      ((i as i32, j as i32), Pixel::from_char(c).unwrap())
+    })
+    .collect::<Vec<_>>()
+  ).collect::<ImageLookup>();
+  let image = Image { lookup, offset: -1, dim };
+  Puzzle { algo, image }
+}
+
+fn edge(algo: &Vec<Pixel>, depth: usize) -> isize {
+  match algo[0] {
+    Pixel::Dark => 0isize,
+    Pixel::Light => (depth & 1) as isize
+  }
+}
+
+fn enhance(image: &mut Image, algo: &Vec<Pixel>, edge: isize) {
+  let (i, j) = image.dim;
+  let offset = image.offset;
+  let mut lookup_next = ImageLookup::new();
+  for i in offset..=(i as i32 - offset) {
+    for j in offset..=(j as i32 - offset) {
+      let mut value = 0;
+      for dx in [-1, 0, 1] {
+        for dy in [-1, 0, 1] {
+          let next = image.lookup.get(&(i + dx, j + dy)).map_or(edge, |&val| (val as isize));
+          value = (value << 1) | next;
+        }
+      }
+      lookup_next.insert((i, j), *algo.get(value as usize).unwrap());
+    }
+  }
+  image.lookup = lookup_next;
+  image.offset = offset - 1;
+}
+
+fn step(puzzle: &mut Puzzle, count: usize) -> usize {
+  for depth in 0..count {
+    let border = edge(&puzzle.algo, depth);
+    enhance(&mut puzzle.image, &puzzle.algo, border);
+  }
+  puzzle.image.lookup.values().filter(|&v| matches!(*v, Pixel::Light)).count()
+}
+
 pub fn part1(input: String) {
-  todo!();
+  let mut input = get_input(&input);
+  let part1 = step(&mut input, 2);
+  dbg!(part1);
 }
 
 pub fn part2(input: String) {
-  todo!();
+  let mut input = get_input(&input);
+  let part2 = step(&mut input, 50);
+  dbg!(part2);
 }
--- 2021/src/day21.rs
+++ 2021/src/day21.rs
@@ -1,7 +1,62 @@
+use std::collections::HashMap;
+
+fn get_start(input: &str) -> (usize, usize) {
+  let mut lines = input.lines();
+  (
+    lines.next().unwrap().split_once(": ").unwrap().1.parse::<usize>().unwrap(),
+    lines.next().unwrap().split_once(": ").unwrap().1.parse::<usize>().unwrap()
+  )
+}
+
 pub fn part1(input: String) {
-  todo!();
+  let (p1, p2) = get_start(&input);
+  let mut rolls = 0usize;
+  let mut players: [(usize, usize); 2] = [(p1, 0), (p2, 0)];
+  'game: loop {
+    for player in players.iter_mut() {
+      let steps = 3 * rolls + 6;
+      rolls += 3;
+      player.0 = (steps + player.0 - 1) % 10 + 1;
+      player.1 += player.0;
+      if player.1 >= 1000 {
+        break 'game;
+      }
+    }
+  }
+  let part1 = rolls * players[0].1.min(players[1].1);
+  dbg!(part1);
+}
+
+fn recursive_step(pos1: usize, pos2: usize, score1: usize, score2: usize, cache: &mut HashMap<(usize, usize, usize, usize), (i128, i128)>) -> (i128, i128) {
+  let key = (pos1, pos2, score1, score2);
+  if cache.contains_key(&key) {
+    return cache[&(pos1, pos2, score1, score2)];
+  } else {
+    let ret;
+    if score2 >= 21 {
+      ret = (0, 1)
+    } else {
+      let rolls = [3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 6, 5, 6, 7, 6, 7, 8, 5, 6, 7, 6, 7, 8, 7, 8, 9];
+      let mut wins1 = 0;
+      let mut wins2 = 0;
+      for next in rolls {
+        let next_pos = (pos1 + next - 1) % 10 + 1;
+        let next_score = score1 + next_pos;
+        let rec = recursive_step(pos2, next_pos, score2, next_score, cache);
+        wins1 += rec.1;
+        wins2 += rec.0;
+      }
+      ret = (wins1, wins2)
+    }
+    cache.insert(key, ret);
+    return ret;
+  }
 }
 
 pub fn part2(input: String) {
-  todo!();
+  let (p1, p2) = get_start(&input);
+  let mut cache = HashMap::new();
+  let universes = recursive_step(p1, p2, 0, 0, &mut cache);
+  let part2 = universes.0.max(universes.1);
+  dbg!(part2);
 }
