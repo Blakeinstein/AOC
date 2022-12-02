c3282856f829d812ef54eaa2a9da367ddca01c86
Blakeinstein <rishi1998@gmail.com>
2022-12-01 17:53:38 -0700

    feat: day1

--- 2022/src/day01.rs
+++ 2022/src/day01.rs
@@ -1,7 +1,25 @@
 pub fn part1(input: String) { 
-  todo!();
+  let inventories = input.split("\n\n");
+  let max = inventories.map(|inventory| {
+    inventory
+      .split("\n")
+      .filter(|&s| !s.is_empty())
+      .map(|s| s.parse::<u64>().unwrap())
+      .sum::<u64>()
+  }).max().unwrap();
+  dbg!(max);
 }
 
 pub fn part2(input: String) {
-  todo!();
+  let inventories = input.split("\n\n");
+  let mut rations = inventories.map(|inventory| {
+    inventory
+      .split("\n")
+      .filter(|&s| !s.is_empty())
+      .map(|s| s.parse::<u64>().unwrap())
+      .sum::<u64>()
+  }).collect::<Vec<u64>>();
+  rations.sort();
+  let max: u64 = rations.iter().rev().take(3).sum();
+  dbg!(max);
 }
