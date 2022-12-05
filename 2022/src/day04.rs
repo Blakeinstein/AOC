40223b2cccd24f6e058579a52fde479560e6517b
Blakeinstein <rishi1998@gmail.com>
2022-12-04 17:42:38 -0700

    feat: day4 done

--- 2022/src/day04.rs
+++ 2022/src/day04.rs
@@ -1,7 +1,29 @@
-pub fn part1(input: String) { 
-  todo!();
+pub fn part1(input: String) {
+  let ans = input
+  .split("\n")
+  .filter(|s| !s.is_empty())
+  .filter(|pair| {
+    let values: Vec<u32> = pair.split(&['-', ','][..]).map(
+      |s| s.parse::<u32>().unwrap()
+    ).collect();
+    (values[0] <= values[2] && values[3] <= values[1]) ||
+    (values[0] >= values[2] && values[3] >= values[1])
+  })
+  .count();
+  dbg!(ans);
 }
 
 pub fn part2(input: String) {
-  todo!();
+  let ans = input
+  .split("\n")
+  .filter(|s| !s.is_empty())
+  .filter(|pair| {
+    let values: Vec<u32> = pair.split(&['-', ','][..]).map(
+      |s| s.parse::<u32>().unwrap()
+    ).collect();
+    values[0] <= values[3] &&
+    values[2] <= values[1]
+  })
+  .count();
+  dbg!(ans);
 }
