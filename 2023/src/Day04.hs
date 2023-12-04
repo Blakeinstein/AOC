8ae1977c1660bc068b906dd6216547634d456581
Blakeinstein <rishi1998@gmail.com>
2023-12-04 15:17:43 -0700

    day 5

--- 2023/src/Day04.hs
+++ 2023/src/Day04.hs
@@ -1,12 +1,52 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Day04 (solve) where
 
-part1 :: [String] -> Int
-part1 _ = 0
+import Control.Applicative (Alternative, asum, many, some)
+import Control.Monad (void)
 
-part2 :: [String] -> Int
-part2 _ = 0
+import Data.Char (isDigit)
+import Data.Maybe (mapMaybe)
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+import Text.Regex.Applicative (RE, sym, string, psym, (=~))
+import Text.Regex.Applicative.Common (decimal)
+import Debug.Trace (trace)
+
+type IntSet = S.Set Int
+data Card a = Card Int a a deriving (Show, Functor)
+type Inp = [Card IntSet]
+
+type Parser t = RE Char t
+
+spaces :: Parser ()
+spaces = void (some (sym ' '))
+
+intSet :: Parser IntSet
+intSet = S.fromList <$> some number
+  where number = spaces *> decimal
+
+line :: Parser (Card IntSet)
+line = Card <$> (string "Card" *> spaces *> decimal) <* string ":" <*> intSet <* string " |" <*> intSet
+
+dbg :: Int -> Int
+dbg a = trace (show a) a
+
+part1 :: Inp -> Int
+part1 = sum . map value
+  where value (Card _ a b) | S.null winners = 0
+                           | otherwise = 2 ^ (S.size winners - 1)
+          where winners = S.intersection a b
+
+part2 :: Inp -> Int
+part2 cards = M.size netCards + sum (M.elems netCards)
+  where netCards = M.fromList entries
+        entries = do
+          Card id win ours <- cards
+          let winners = S.size $ S.intersection win ours
+          pure (id, winners + sum [netCards M.! (id + i) | i <- [1..winners]])
 
 solve :: String -> IO ()
-solve input = putStrLn "--- Day 01 ---" >> print (part1 $ p input) >> print (part2 $ p input)
+solve input = putStrLn "--- Day 04 ---" >> print (part1 $ p input) >> print (part2 $ p input)
   where
-    p = map read . lines
+    p = mapMaybe (=~ line) . lines
