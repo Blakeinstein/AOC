79453a868e6413467df36186a321487f90c1d4e6
Blakeinstein <rishi1998@gmail.com>
2023-12-16 15:59:12 +0530

    day 12 and 13

--- 2023/src/Day12.hs
+++ 2023/src/Day12.hs
@@ -1,12 +1,77 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Day12 (solve) where
 
-part1 :: [String] -> Int
-part1 _ = 0
+import Control.Applicative (Alternative, many)
+import Control.Monad.State
 
-part2 :: [String] -> Int
-part2 _ = 0
+import Data.List (nub, intercalate)
+import Data.Maybe (mapMaybe)
+import qualified Data.Map as M
+
+import Text.Regex.Applicative ( RE, sym, (=~), (<|>) )
+import Text.Regex.Applicative.Common (decimal, signed)
+import Debug.Trace (trace)
+
+sepBy :: Alternative f => f a -> f b -> f [a]
+p `sepBy` sep = (:) <$> p <*> many (sep *> p)
+
+type Parser a = RE Char a
+
+data Cond = Op | Dam | Un deriving (Show, Eq, Ord)
+type Rec = ([Cond], [Int])
+
+type Inp = [Rec]
+
+type Memo = M.Map ([Cond], [Int]) Int
+type MemoState = State Memo Int
+
+notAfter :: Int -> Cond -> [Cond] -> Bool
+notAfter x c s = none c (take 1 (drop x s))
+
+only :: Cond -> [Cond] -> Bool
+only c = all (== c) . nub
+
+none :: Cond -> [Cond] -> Bool
+none c = notElem c . nub
+
+possible' :: ([Cond] -> [Int] -> MemoState) -> [Cond] -> [Int] -> MemoState
+possible' f [] [] = pure 1
+possible' f [] [x] = pure 0
+possible' f s [] = pure (if none Dam s then 1 else 0)
+possible' f (Op:rs) xs = f rs xs
+possible' f (Un:rs) xs = f rs xs >>= \a -> f (Dam:rs) xs >>= \b -> pure (a + b)
+possible' f s (x:rx) | length s >= x && none Op (take x s) && notAfter x Dam s
+  = f (drop (x + 1) s) rx
+possible' _ _ _= pure 0
+
+possible :: [Cond] -> [Int] -> Int
+possible conds = flip evalState M.empty . memo possible' conds 
+  where 
+    memo mf s xs = let key = (s, xs) in gets (M.lookup key) >>= \case
+      Just v -> pure v
+      Nothing -> mf (memo mf) s xs >>= \v -> modify (M.insert key v) >> pure v
+
+part1 :: Inp -> Int
+part1 = sum . map (uncurry possible)
+
+unfold :: Inp -> Inp
+unfold = map f
+  where f (s, xs) = (intercalate [Un] (replicate 5 s), concat (replicate 5 xs))
+
+part2 :: Inp -> Int
+part2 = part1 . unfold
+
+conditions :: Parser [Cond]
+conditions = many $ (Op <$ sym '.') <|> (Dam <$ sym '#') <|> (Un <$ sym '?')
+
+damages :: Parser [Int]
+damages = decimal `sepBy` sym ','
+
+parse :: Parser Rec
+parse = (,) <$> (conditions <* sym ' ') <*> damages
 
 solve :: String -> IO ()
-solve input = putStrLn "--- Day 01 ---" >> print (part1 $ p input) >> print (part2 $ p input)
+solve input = putStrLn "--- Day 12 ---" >> print (part1 $ p input) >> print (part2 $ p input)
   where
-    p = map read . lines
+    p = mapMaybe (=~ parse) . lines
--- 2023/src/Day13.hs
+++ 2023/src/Day13.hs
@@ -1,12 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day13 (solve) where
 
-part1 :: [String] -> Int
-part1 _ = 0
+import Control.Applicative ( Alternative, many, some )
 
-part2 :: [String] -> Int
-part2 _ = 0
+import Data.Maybe (fromMaybe, fromJust)
+import Data.List (transpose, find)
+
+import Text.Regex.Applicative ( RE, sym, (=~), (<|>), string )
+import Debug.Trace (trace)
+import Coord (Coord(..))
+
+sepBy :: Alternative f => f a -> f b -> f [a]
+p `sepBy` sep = (:) <$> p <*> many (sep *> p)
+
+dbg :: Show a => a -> a
+dbg a = trace (show a) a
+
+type Parser a = RE Char a
+
+data Node = Ash | Rock deriving (Show, Eq, Ord)
+type Map = [[Node]]
+
+type Inp = [Map]
+
+diff :: Map -> Map -> Int
+diff pat = sum . zipWith f pat
+  where f r = sum . zipWith (\c1 c2 -> if c1 == c2 then 0 else 1) r
+
+reflection :: Int -> Map -> Maybe Int
+reflection delta pat = find f [1..length pat - 1]
+  where f row = let (a, b) = splitAt row pat in diff (reverse a) b == delta
+
+summarize :: Int -> Map -> Int
+summarize delta = fromJust . f
+  where f pat = (*100) <$> reflection delta pat <|> reflection delta (transpose pat)
+
+summarizeAll :: Int -> Inp -> Int
+summarizeAll delta = sum . map (summarize delta)
+
+part1 :: Inp -> Int
+part1 = summarizeAll 0
+
+part2 :: Inp -> Int
+part2 = summarizeAll 1
+
+node :: Parser Node
+node = (Ash <$ ".") <|> (Rock <$ "#")
+
+nodeLine :: Parser [Node]
+nodeLine = some node <* "\n"
+
+parseMap :: Parser Map
+parseMap = some nodeLine
+
+parse :: Parser Inp
+parse = parseMap `sepBy` "\n"
 
 solve :: String -> IO ()
-solve input = putStrLn "--- Day 01 ---" >> print (part1 $ p input) >> print (part2 $ p input)
+solve input = putStrLn "--- Day 13 ---" >> print (part1 $ p input) >> print (part2 $ p input)
   where
-    p = map read . lines
+    p = fromMaybe (error "No Parse") . (=~ parse)
+
