c178559dd9af20b7e5c0ecde94ee40ef81f06ef2
Rishikesh Anand <rishi1998@gmail.com>
2024-12-07 17:46:36 -0700

    chore: I forgot to push

--- 2024/days/day_06/main.go
+++ 2024/days/day_06/main.go
@@ -14,12 +14,135 @@ func Run(input []string, mode int) {
 	}
 }
 
+type Position struct {
+	x int
+	y int
+}
+
+var directions = [4]Position{
+	{0, -1},
+	{1, 0},
+	{0, 1},
+	{-1, 0},
+}
+
+type Guard struct {
+	position  Position
+	direction int
+}
+
+const obstacle = '#'
+
+func parseInput(input []string) [][]rune {
+	grid := make([][]rune, len(input))
+	for i, line := range input {
+		grid[i] = []rune(line)
+	}
+	return grid
+}
+
+const startChar = '^'
+
+func findInitialPosition(grid [][]rune) (Position, error) {
+	for i, row := range grid {
+		for j, cell := range row {
+			if cell == startChar {
+				return Position{j, i}, nil
+			}
+		}
+	}
+	return Position{}, fmt.Errorf("initial position not found")
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	grid := parseInput(input)
+
+	width := len(grid)
+	height := len(grid[0])
+
+	var uniquePosition = make(map[Position]bool)
+
+	initialPosition, err := findInitialPosition(grid)
+
+	if err != nil {
+		print(err)
+		return ""
+	}
+
+	guard := Guard{initialPosition, 0}
+
+	for {
+		uniquePosition[guard.position] = true
+		nextX := guard.position.x + directions[guard.direction].x
+		nextY := guard.position.y + directions[guard.direction].y
+
+		if !(nextX >= 0 && nextX < width && nextY >= 0 && nextY < height) {
+			break
+		}
+
+		if grid[nextY][nextX] == obstacle {
+			guard.direction = (guard.direction + 1) % 4
+			continue
+		}
+
+		guard.position.x = nextX
+		guard.position.y = nextY
+	}
+
+	return fmt.Sprintf("%d", len(uniquePosition))
+}
+
+func isLoop(initalPos Position, grid [][]rune) bool {
+	history := make(map[Guard]bool)
+	width := len(grid)
+	height := len(grid[0])
+
+	guard := Guard{initalPos, 0}
+
+	for {
+		if _, ok := history[guard]; ok {
+			return true
+		}
+
+		history[guard] = true
+		nextX := guard.position.x + directions[guard.direction].x
+		nextY := guard.position.y + directions[guard.direction].y
+
+		if !(nextX >= 0 && nextX < width && nextY >= 0 && nextY < height) {
+			return false
+		}
+
+		if grid[nextY][nextX] == obstacle {
+			guard.direction = (guard.direction + 1) % 4
+			continue
+		}
+
+		guard.position.x = nextX
+		guard.position.y = nextY
+	}
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	grid := parseInput(input)
+
+	initialPos, _ := findInitialPosition(grid)
+
+	loopCount := 0
+
+	for i, row := range grid {
+		for j, cell := range row {
+			if cell == obstacle {
+				continue
+			}
+			grid[i][j] = obstacle
+			if isLoop(initialPos, grid) {
+				loopCount++
+			}
+			grid[i][j] = cell
+		}
+	}
+
+	return fmt.Sprintf("%d", loopCount)
 }
--- 2024/go.mod
+++ 2024/go.mod
@@ -1,3 +1,8 @@
 module github.com/blakeinstein/AOC/2024
 
 go 1.21.4
+
+require (
+	github.com/samber/lo v1.47.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
