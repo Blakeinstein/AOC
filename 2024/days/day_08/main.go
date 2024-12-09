a6c309957dad7fad3a465a23ee3c4e06d46cd4d1
Rishikesh Anand <rishi1998@gmail.com>
2024-12-09 15:59:52 -0700

    chore: dont party hard on weekends

--- 2024/days/day_08/main.go
+++ 2024/days/day_08/main.go
@@ -14,12 +14,115 @@ func Run(input []string, mode int) {
 	}
 }
 
+type Point struct {
+	x int
+	y int
+}
+
+type Node struct {
+	point Point
+	char  rune
+}
+
+func parseInput(input []string) []Node {
+	towers := []Node{}
+
+	for y, line := range input {
+		for x, char := range line {
+			if char != '.' {
+				towers = append(towers, Node{Point{x, y}, char})
+			}
+		}
+	}
+
+	return towers
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	height := len(input)
+	width := len(input[0])
+
+	towers := parseInput(input)
+
+	antinodes := make(map[Point]bool)
+
+	for i, node1 := range towers {
+		for _, node2 := range towers[i+1:] {
+			if node1.char != node2.char {
+				continue
+			}
+
+			p1 := node1.point
+			p2 := node2.point
+
+			dx := 2 * (p2.x - p1.x)
+			dy := 2 * (p2.y - p1.y)
+			// 2
+
+			px := p2.x - dx
+			py := p2.y - dy
+
+			if px >= 0 && px < width && py >= 0 && py < height {
+				antinodes[Point{px, py}] = true
+			}
+
+			px = p1.x + dx
+			py = p1.y + dy
+
+			if px >= 0 && px < width && py >= 0 && py < height {
+				antinodes[Point{px, py}] = true
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d", len(antinodes))
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	height := len(input)
+	width := len(input[0])
+
+	towers := parseInput(input)
+
+	antinodes := make(map[Point]bool)
+
+	for i, node1 := range towers {
+		for _, node2 := range towers[i+1:] {
+			if node1.char != node2.char {
+				continue
+			}
+
+			antinodes[node1.point] = true
+			antinodes[node2.point] = true
+
+			p1 := node1.point
+			p2 := node2.point
+
+			dx := (p2.x - p1.x)
+			dy := (p2.y - p1.y)
+			// 2
+
+			px := p2.x - 2*dx
+			py := p2.y - 2*dy
+
+			for px >= 0 && px < width && py >= 0 && py < height {
+				antinodes[Point{px, py}] = true
+				px -= dx
+				py -= dy
+			}
+
+			px = p1.x + 2*dx
+			py = p1.y + 2*dy
+
+			for px >= 0 && px < width && py >= 0 && py < height {
+				antinodes[Point{px, py}] = true
+				py += dy
+				px += dx
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d", len(antinodes))
 }
--- 2024/days/day_09/main.go
+++ 2024/days/day_09/main.go
@@ -2,6 +2,7 @@ package day_09
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // Run function of the daily challenge
@@ -16,10 +17,115 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	line := input[0]
+
+	blocks := []int{}
+	isBlock := true
+	blockCount := 0
+
+	for _, char := range line {
+		count, err := strconv.Atoi(string(char))
+		if err != nil {
+			fmt.Println("Error converting rune to int:", err)
+			return ""
+		}
+
+		if isBlock {
+			for i := 0; i < count; i++ {
+				blocks = append(blocks, blockCount)
+			}
+			blockCount++
+		} else {
+			for i := 0; i < count; i++ {
+				blocks = append(blocks, -1)
+			}
+		}
+
+		isBlock = !isBlock
+	}
+
+	idx := 0
+	popIdx := len(blocks) - 1
+
+	for idx < popIdx {
+		if blocks[popIdx] == -1 {
+			popIdx--
+			continue
+		}
+		if blocks[idx] != -1 {
+			idx++
+			continue
+		}
+		blocks[idx], blocks[popIdx] = blocks[popIdx], blocks[idx]
+	}
+
+	sum := 0
+
+	for i, b := range blocks {
+		if b == -1 {
+			continue
+		}
+		sum += i * b
+	}
+
+	return strconv.Itoa(sum)
+}
+
+type File struct {
+	size     int
+	position int
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	line := input[0]
+
+	files := []File{}
+	spaces := []File{}
+
+	isFile := true
+
+	pos := 0
+
+	for _, char := range line {
+		digit, err := strconv.Atoi(string(char))
+
+		if err != nil {
+			fmt.Println("Error converting rune to int:", err)
+			return ""
+		}
+
+		if isFile {
+			files = append(files, File{digit, pos})
+		} else {
+			spaces = append(spaces, File{digit, pos})
+		}
+		pos += digit
+
+		isFile = !isFile
+	}
+
+	idx := len(files) - 1
+
+	for idx >= 1 {
+		for i := range spaces {
+			if spaces[i].position > files[idx].position {
+				break
+			}
+			if spaces[i].size >= files[idx].size {
+				spaces[i].size -= files[idx].size
+				files[idx].position = spaces[i].position
+				spaces[i].position += files[idx].size
+				break
+			}
+		}
+		idx--
+	}
+
+	sum := 0
+
+	for i, f := range files {
+		sum += f.size * i * (2*f.position + f.size - 1) / 2
+	}
+	return fmt.Sprintf("%d", sum)
 }
