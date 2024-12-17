38bb59b29163675a105df2ca9e86e842849e9ca7
Rishikesh Anand <rishi1998@gmail.com>
2024-12-17 11:54:17 -0700

    chore: 16 and 17

--- 2024/days/day_16/main.go
+++ 2024/days/day_16/main.go
@@ -2,6 +2,7 @@ package day_16
 
 import (
 	"fmt"
+	"math"
 )
 
 // Run function of the daily challenge
@@ -14,12 +15,143 @@ func Run(input []string, mode int) {
 	}
 }
 
+type Point struct {
+	x int
+	y int
+}
+
+type Node struct {
+	p Point
+	d Point
+}
+
+type Input struct {
+	m [][]rune
+	s Point
+	e Point
+}
+
+func parseInput(input []string) Input {
+	var m [][]rune
+	var s Point
+	var e Point
+	for y, line := range input {
+		var row []rune
+		for x, r := range line {
+			row = append(row, r)
+			if r == 'S' {
+				s = Point{x, y}
+			}
+			if r == 'E' {
+				e = Point{x, y}
+			}
+		}
+		m = append(m, row)
+	}
+	return Input{m, s, e}
+}
+
+var directions = []Point{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+
+type State struct {
+	node Node
+	cost int
+	path []Point
+}
+
+func getNeighbours(n Node) (nbrs []Node) {
+	nbrs = make([]Node, 0, 4)
+	currDir, currPos := n.d, n.p
+	opp := Point{-currDir.x, -currDir.y}
+
+	for _, dir := range directions {
+		if dir == opp {
+			continue
+		}
+		nP := Point{x: currPos.x + dir.x, y: currPos.y + dir.y}
+		nbrs = append(nbrs, Node{nP, dir})
+	}
+	return
+}
+
+func bfs(inp Input) (int, int) {
+	queue := []State{{Node{inp.s, directions[1]}, 0, []Point{inp.s}}}
+	visited := make(map[Node]int)
+	minCost := math.MaxInt
+	mapPath := make(map[Point]int)
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if current.cost > minCost {
+			continue
+		}
+
+		if current.node.p == inp.e {
+			if current.cost <= minCost {
+				minCost = current.cost
+				for _, p := range current.path {
+					mapPath[p] = current.cost
+				}
+			}
+			continue
+		}
+
+		for _, nP := range getNeighbours(current.node) {
+			if nP.p.x < 0 || nP.p.x >= len(inp.m[0]) || nP.p.y < 0 || nP.p.y >= len(inp.m) || inp.m[nP.p.y][nP.p.x] == '#' {
+				continue
+			}
+
+			nextCost := current.cost + 1
+			if nP.d != current.node.d {
+				nextCost += 1000
+			}
+
+			if v, ok := visited[nP]; ok {
+				if v < nextCost {
+					continue
+				}
+			}
+
+			visited[nP] = nextCost
+			nPath := make([]Point, len(current.path))
+			copy(nPath, current.path)
+			queue = append(queue, State{
+				nP,
+				nextCost,
+				append(nPath, nP.p),
+			})
+		}
+	}
+
+	if minCost == math.MaxInt {
+		return 0, 0
+	}
+
+	count := 0
+
+	for _, cost := range mapPath {
+		if cost == minCost {
+			count++
+		}
+	}
+
+	return minCost, count
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	inp := parseInput(input)
+
+	minCost, _ := bfs(inp)
+	return fmt.Sprintf("%d", minCost)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	inp := parseInput(input)
+
+	_, count := bfs(inp)
+	return fmt.Sprintf("%d", count)
 }
--- 2024/days/day_17/main.go
+++ 2024/days/day_17/main.go
@@ -2,6 +2,11 @@ package day_17
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
 )
 
 // Run function of the daily challenge
@@ -14,12 +19,98 @@ func Run(input []string, mode int) {
 	}
 }
 
+type Registers struct {
+	a  uint64
+	b  uint64
+	c  uint64
+	ip int
+}
+
+func getOpd(r *Registers, opd rune) uint64 {
+	switch opd {
+	case '4':
+		return r.a
+	case '5':
+		return r.b
+	case '6':
+		return r.c
+	case '7':
+		fmt.Println("saw 7")
+		return 1
+	default:
+		return uint64(opd - '0')
+	}
+}
+
+func performOperation(r *Registers, op, opd rune, stdout *[]int) {
+	switch op {
+	case '0':
+		r.a = r.a >> getOpd(r, opd)
+	case '1':
+		r.b = r.b ^ uint64(opd-'0')
+	case '2':
+		r.b = getOpd(r, opd) & 7
+	case '3':
+		if r.a != 0 {
+			r.ip = int(opd - '0')
+			return
+		}
+	case '4':
+		r.b = r.b ^ r.c
+	case '5':
+		*stdout = append(*stdout, int(getOpd(r, opd)&7))
+	case '6':
+		r.b = r.a >> getOpd(r, opd)
+	case '7':
+		r.c = r.a >> getOpd(r, opd)
+	}
+	r.ip += 2
+}
+
+func parseInput(input []string) (r Registers, ops []rune) {
+	ra, _ := strconv.Atoi(strings.Split(input[0], ": ")[1])
+	rb, _ := strconv.Atoi(strings.Split(input[1], ": ")[1])
+	rc, _ := strconv.Atoi(strings.Split(input[2], ": ")[1])
+
+	r = Registers{uint64(ra), uint64(rb), uint64(rc), 0}
+	ops = lo.Map(strings.Split(strings.Split(input[4], ": ")[1], ","), func(s string, _ int) rune { return []rune(s)[0] })
+
+	return
+}
+
+func perfOps(r *Registers, ops []rune) (stdout []int) {
+	for r.ip < len(ops) {
+		performOperation(r, ops[r.ip], ops[r.ip+1], &stdout)
+	}
+	return
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	r, ops := parseInput(input)
+	stdout := perfOps(&r, ops)
+	return strings.Trim(strings.Replace(fmt.Sprint(stdout), " ", ",", -1), "[]")
+}
+
+func seedOps(ops []rune, seed uint64) []int {
+	r := Registers{seed, 0, 0, 0}
+	return perfOps(&r, ops)
+}
+
+func findMirror(ops []rune) (seed uint64) {
+	expected_out := lo.Map(ops, func(op rune, _ int) int { return int(op - '0') })
+	for itr := len(ops) - 1; itr >= 0; itr-- {
+		seed <<= 3
+		for !slices.Equal(seedOps(ops, seed), expected_out[itr:]) {
+			seed++
+		}
+	}
+	return
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	_, ops := parseInput(input)
+	seed := findMirror(ops)
+	return fmt.Sprintf("%v", seed)
 }
