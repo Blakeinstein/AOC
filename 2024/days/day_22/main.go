59caee6ca73b569fde47145a693c867bd95ab079
Rishikesh Anand <rishi1998@gmail.com>
2024-12-23 11:06:58 -0700

    day 21 and 22

--- 2024/days/day_22/main.go
+++ 2024/days/day_22/main.go
@@ -2,6 +2,7 @@ package day_22
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // Run function of the daily challenge
@@ -14,12 +15,84 @@ func Run(input []string, mode int) {
 	}
 }
 
+func parseInput(input []string) (buyerSecrets []int) {
+	buyerSecrets = make([]int, len(input))
+	for i, line := range input {
+		buyerSecrets[i], _ = strconv.Atoi(line)
+	}
+	return
+}
+
+func mix(secret, next int) int {
+	return (secret ^ next)
+}
+
+func prune(secret int) int {
+	return secret & 0xFFFFFF
+}
+
+func getNextSecret(secret int) int {
+	result := secret
+	result = prune(mix(result, result<<6))
+	result = prune(mix(result, result>>5))
+	result = prune(mix(result, result<<11))
+	return result
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	sum := 0
+	buyerSecrets := parseInput(input)
+	for _, init := range buyerSecrets {
+		secret := init
+		for i := 0; i < 2000; i++ {
+			secret = getNextSecret(secret)
+		}
+		sum += secret
+	}
+	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	buyerSecrets := parseInput(input)
+
+	changeMap := map[[4]int]int{}
+	for _, secret := range buyerSecrets {
+		changes := make([]int, 2000)
+		currentPrice := secret % 10
+
+		currentChangeMap := map[[4]int]int{}
+
+		for i := range 2000 {
+			secret = getNextSecret(secret)
+			newPrice := secret % 10
+			diff := newPrice - currentPrice
+
+			changes[i] = diff
+			if i >= 3 {
+				ch := [4]int(changes[i-3 : i+1])
+				if _, ok := currentChangeMap[ch]; !ok {
+					currentChangeMap[ch] = newPrice
+				}
+			}
+
+			currentPrice = newPrice
+		}
+		for changeSeq, bananas := range currentChangeMap {
+			if _, ok := changeMap[changeSeq]; !ok {
+				changeMap[changeSeq] = 0
+			}
+			changeMap[changeSeq] += bananas
+		}
+	}
+
+	// find max value in sequences map
+	maxBananas := -1
+	for _, v := range changeMap {
+		if v > maxBananas {
+			maxBananas = v
+		}
+	}
+	return strconv.Itoa(maxBananas)
 }
