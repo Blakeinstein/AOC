5988c1e35a15e8051abff8ddbc90306d73e86c09
Rishikesh Anand <rishi1998@gmail.com>
2024-12-23 11:54:26 -0700

    damn day 23 was easy

--- 2024/days/day_23/main.go
+++ 2024/days/day_23/main.go
@@ -2,6 +2,8 @@ package day_23
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -14,12 +16,83 @@ func Run(input []string, mode int) {
 	}
 }
 
+func parseInput(input []string) (graph map[string][]string) {
+	graph = make(map[string][]string)
+	for _, line := range input {
+		nodes := strings.Split(line, "-")
+		graph[nodes[0]] = append(graph[nodes[0]], nodes[1])
+		graph[nodes[1]] = append(graph[nodes[1]], nodes[0])
+	}
+	return
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	graph := parseInput(input)
+
+	count := 0
+	for node1, v1 := range graph {
+		for _, node2 := range v1 {
+			for _, node3 := range graph[node2] {
+				if node3 == node1 {
+					continue
+				}
+				for _, node4 := range graph[node3] {
+					if node4 == node1 {
+						if node1[0] == 't' || node2[0] == 't' || node3[0] == 't' {
+							count++
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return fmt.Sprintf("%v", count/6)
+}
+
+func contains(slice []string, item string) bool {
+	for _, s := range slice {
+		if s == item {
+			return true
+		}
+	}
+	return false
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	graph := parseInput(input)
+
+	var maxClique []string
+
+	var findClique func([]string, []string)
+	findClique = func(potentialClique, remainingNodes []string) {
+		if len(remainingNodes) == 0 {
+			if len(potentialClique) > len(maxClique) {
+				maxClique = append([]string{}, potentialClique...)
+			}
+			return
+		}
+
+		for i, node := range remainingNodes {
+			newPotentialClique := append(potentialClique, node)
+			newRemainingNodes := []string{}
+			for _, neighbor := range graph[node] {
+				if contains(remainingNodes[i+1:], neighbor) {
+					newRemainingNodes = append(newRemainingNodes, neighbor)
+				}
+			}
+			findClique(newPotentialClique, newRemainingNodes)
+		}
+	}
+
+	nodes := make([]string, 0, len(graph))
+	for node := range graph {
+		nodes = append(nodes, node)
+	}
+	findClique([]string{}, nodes)
+
+	sort.Strings(maxClique)
+	return strings.Join(maxClique, ",")
 }
