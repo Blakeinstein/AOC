705934bf4ea88c2608c17f1ad88d816926006fcd
Rishikesh Anand <rishi1998@gmail.com>
2025-12-03 12:58:48 +0530

    feat: solve day 3

--- 2025/days/day03.jl
+++ 2025/days/day03.jl
@@ -23,24 +23,18 @@ real_input_fp = `../input/day$DAY.txt`
 run(`aoc -y $YEAR -d $DAY download -I -i $real_input_fp`)
 
 # ╔═╡ ea4bd3be-5078-4e13-8373-6804189aae80
-test_input = """L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82"""
+test_input = """987654321111111 811111111111119 234234234234278 818181911112111"""
 
 # ╔═╡ 649d2ff5-f85a-43ed-8993-81a51dda4ad1
 # ╠═╡ show_logs = false
 real_input = read(`cat $real_input_fp`, String)
 
 # ╔═╡ 25c1db54-f713-416e-bee4-efc299a9bac6
-function parse_input(input_str)
-	return input_str
+function parse_input(input_str::String)::Vector{Vector{UInt8}}
+	return map(
+		x -> map(y -> parse(UInt8, y), collect(x)),
+		split(input_str)
+	)
 end
 
 # ╔═╡ b24a080f-199a-4632-86a0-7af867567c2e
@@ -49,9 +43,23 @@ function solver(input_parser, part_solver, input)
 	return string(part_solver(parsed_input))
 end
 
+# ╔═╡ a9dd5194-b718-4030-b645-3df968d4730a
+function calc_max_joltage(battery::Vector{UInt8})::Number
+	size = length(battery)
+	curr_max = battery[1] * 10 + battery[end]
+	for i in 1:size, j in i+1:size
+		curr_max = max(curr_max, battery[i] * 10 + battery[j])
+	end
+	return curr_max
+end
+
 # ╔═╡ 9efaf158-c88a-47a4-a0b6-86c5227d5daa
-function solve_part1(input::String)::Number
-	return length(input)
+function solve_part1(batteries::Vector{Vector{UInt8}})::Number
+	joltage = 0
+	for battery in batteries
+		joltage += calc_max_joltage(battery)
+	end
+	return joltage
 end
 
 # ╔═╡ c34928c8-a273-4d63-8fa5-e3de89fff8d7
@@ -60,9 +68,26 @@ solver(parse_input, solve_part1, test_input)
 # ╔═╡ 6f275cf8-4367-4398-aa21-8f1d9cd8c36e
 solver(parse_input, solve_part1, real_input)
 
+# ╔═╡ 20a788b1-cba1-44c1-b3ed-27f4dd220195
+function calc_max_12_joltage(battery::Vector{UInt8})::Number
+	size = length(battery)
+    to_remove = size - 12
+    stack = UInt8[]
+    
+    for digit in battery
+        while to_remove > 0 && !isempty(stack) && stack[end] < digit
+            pop!(stack)
+            to_remove -= 1
+        end
+        push!(stack, digit)
+    end
+    
+    return parse(Int, join(stack[1:12]))
+end
+
 # ╔═╡ f7bb2230-22ba-4b40-8af2-dd7226e0a9b2
-function solve_part2(input::String)::Number
-	return length(input)
+function solve_part2(batteries::Vector{Vector{UInt8}})::Number
+	sum(map(calc_max_12_joltage, batteries))
 end
 
 # ╔═╡ 154fec09-f761-43aa-aaed-129d4d2f711e
@@ -88,18 +113,20 @@ project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
 """
 
 # ╔═╡ Cell order:
-# ╠═a02868a2-6df2-4d50-8cd0-5ca77c6d8cea
-# ╠═d843733f-a453-44c4-b759-6e84dcb535e5
-# ╠═74bb4e7b-c622-40bc-8ab7-b06068d5595c
-# ╠═5367dbdf-ee9f-4360-a7d3-a8a5988d90fa
-# ╠═8a79b53a-c8fe-11f0-9297-97044163d935
-# ╠═ea4bd3be-5078-4e13-8373-6804189aae80
-# ╠═649d2ff5-f85a-43ed-8993-81a51dda4ad1
+# ╟─a02868a2-6df2-4d50-8cd0-5ca77c6d8cea
+# ╟─d843733f-a453-44c4-b759-6e84dcb535e5
+# ╟─74bb4e7b-c622-40bc-8ab7-b06068d5595c
+# ╟─5367dbdf-ee9f-4360-a7d3-a8a5988d90fa
+# ╟─8a79b53a-c8fe-11f0-9297-97044163d935
+# ╟─ea4bd3be-5078-4e13-8373-6804189aae80
+# ╟─649d2ff5-f85a-43ed-8993-81a51dda4ad1
 # ╠═25c1db54-f713-416e-bee4-efc299a9bac6
 # ╠═b24a080f-199a-4632-86a0-7af867567c2e
+# ╠═a9dd5194-b718-4030-b645-3df968d4730a
 # ╠═9efaf158-c88a-47a4-a0b6-86c5227d5daa
 # ╠═c34928c8-a273-4d63-8fa5-e3de89fff8d7
 # ╠═6f275cf8-4367-4398-aa21-8f1d9cd8c36e
+# ╠═20a788b1-cba1-44c1-b3ed-27f4dd220195
 # ╠═f7bb2230-22ba-4b40-8af2-dd7226e0a9b2
 # ╠═154fec09-f761-43aa-aaed-129d4d2f711e
 # ╠═4b783820-f72d-4ea8-bf8c-bbc2d556484c
