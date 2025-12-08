c1e2eaee2eca5f14f9a41388edc5dd841ecaeb29
Rishikesh Anand <rishi1998@gmail.com>
2025-12-08 09:59:32 +0000

    feat: day 6

--- 2025/days/day06.jl
+++ 2025/days/day06.jl
@@ -23,25 +23,26 @@ real_input_fp = `../input/day$DAY.txt`
 run(`aoc -y $YEAR -d $DAY download -I -i $real_input_fp`)
 
 # ╔═╡ ea4bd3be-5078-4e13-8373-6804189aae80
-test_input = """L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82"""
+test_input = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """
 
 # ╔═╡ 649d2ff5-f85a-43ed-8993-81a51dda4ad1
 # ╠═╡ show_logs = false
 real_input = read(`cat $real_input_fp`, String)
 
 # ╔═╡ 25c1db54-f713-416e-bee4-efc299a9bac6
-function parse_input(input_str)
-	return input_str
+function parse_input(input_str::String)::Tuple{Matrix{Int64}, Vector{Char}}
+	lines = collect(split(strip(input_str), "\n"))
+
+	operators = map(x -> x[1], split(lines[end]))
+	operands = stack(parse.(Int, split(line)) for line in lines[begin:end-1]; dims=1)
+	return (operands, operators)
 end
+
+# ╔═╡ 183f8fdc-cf26-4c08-8558-0c0998e2df47
+parse_input(test_input)
 
 # ╔═╡ b24a080f-199a-4632-86a0-7af867567c2e
 function solver(input_parser, part_solver, input)
@@ -49,9 +50,19 @@ function solver(input_parser, part_solver, input)
 	return string(part_solver(parsed_input))
 end
 
+# ╔═╡ 2a431acd-d32c-4356-8848-d978dc860c00
+function get_op(char::Char)
+	eval(Meta.parse(string(char)))
+end
+
 # ╔═╡ 9efaf158-c88a-47a4-a0b6-86c5227d5daa
-function solve_part1(input::String)::Number
-	return length(input)
+function solve_part1(input::Tuple{Matrix{Int64}, Vector{Char}})::Number
+	ans = 0
+	operands, operators = input
+	for (idx, op) in enumerate(operators)
+		ans += reduce(get_op(op), operands[:, idx])
+	end
+	return ans
 end
 
 # ╔═╡ c34928c8-a273-4d63-8fa5-e3de89fff8d7
@@ -60,48 +71,76 @@ solver(parse_input, solve_part1, test_input)
 # ╔═╡ 6f275cf8-4367-4398-aa21-8f1d9cd8c36e
 solver(parse_input, solve_part1, real_input)
 
+# ╔═╡ 094b4c65-6175-4e1a-82d2-96dbefdb716a
+function max_string_length(strings::Vector{String})::Number
+	maximum(map(length, strings))
+end
+
+# ╔═╡ 75004f0e-5c5e-41a4-9f54-1628a85de8e3
+function parse_input2(input_str::String)::Tuple{Vector{String}, String}
+	lines = collect(split(strip(input_str, ['\n']), "\n"))
+
+	operators = lines[end]
+	operands = lines[begin:end-1]
+	return (operands, operators)
+end
+
+# ╔═╡ c6bf8885-435d-4c3b-8c1f-ea6c32d607db
+function calc_ans(op::Char, start::Number, size::Number, ops::Vector{String})
+	numbers = fill("", size)
+	for i in 1:size
+		for line in ops
+			ch = line[start + i - 1]
+			if ch != ' '
+				numbers[i] *= ch
+			end
+		end
+	end
+	reduce(get_op(op), parse.(Int64, numbers))
+end
+
 # ╔═╡ f7bb2230-22ba-4b40-8af2-dd7226e0a9b2
-function solve_part2(input::String)::Number
-	return length(input)
+function solve_part2(input::Tuple{Vector{String}, String})::Number
+	ans = 0
+	operands, operators = input
+	last_idx = 1
+	last_operand = operators[1]
+	for (i, val) in enumerate(operators[2:end])
+		if val != ' '
+			ans += calc_ans(last_operand, last_idx, i - last_idx, operands)
+			last_idx = i + 1
+			last_operand = val
+		end			
+	end
+	final_idx = length(operators)
+	ans += calc_ans(last_operand, last_idx, final_idx - last_idx + 1, operands)
+	return ans
 end
 
 # ╔═╡ 154fec09-f761-43aa-aaed-129d4d2f711e
-solver(parse_input, solve_part2, test_input)
+solver(parse_input2, solve_part2, test_input)
 
 # ╔═╡ 4b783820-f72d-4ea8-bf8c-bbc2d556484c
-solver(parse_input, solve_part2, real_input)
-
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
-julia_version = "1.12.2"
-manifest_format = "2.0"
-project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
-
-[deps]
-"""
+solver(parse_input2, solve_part2, real_input)
 
 # ╔═╡ Cell order:
-# ╠═a02868a2-6df2-4d50-8cd0-5ca77c6d8cea
-# ╠═d843733f-a453-44c4-b759-6e84dcb535e5
-# ╠═74bb4e7b-c622-40bc-8ab7-b06068d5595c
-# ╠═5367dbdf-ee9f-4360-a7d3-a8a5988d90fa
-# ╠═8a79b53a-c8fe-11f0-9297-97044163d935
+# ╟─a02868a2-6df2-4d50-8cd0-5ca77c6d8cea
+# ╟─d843733f-a453-44c4-b759-6e84dcb535e5
+# ╟─74bb4e7b-c622-40bc-8ab7-b06068d5595c
+# ╟─5367dbdf-ee9f-4360-a7d3-a8a5988d90fa
+# ╟─8a79b53a-c8fe-11f0-9297-97044163d935
 # ╠═ea4bd3be-5078-4e13-8373-6804189aae80
-# ╠═649d2ff5-f85a-43ed-8993-81a51dda4ad1
+# ╟─649d2ff5-f85a-43ed-8993-81a51dda4ad1
 # ╠═25c1db54-f713-416e-bee4-efc299a9bac6
+# ╠═183f8fdc-cf26-4c08-8558-0c0998e2df47
 # ╠═b24a080f-199a-4632-86a0-7af867567c2e
+# ╠═2a431acd-d32c-4356-8848-d978dc860c00
 # ╠═9efaf158-c88a-47a4-a0b6-86c5227d5daa
 # ╠═c34928c8-a273-4d63-8fa5-e3de89fff8d7
 # ╠═6f275cf8-4367-4398-aa21-8f1d9cd8c36e
+# ╠═094b4c65-6175-4e1a-82d2-96dbefdb716a
+# ╠═75004f0e-5c5e-41a4-9f54-1628a85de8e3
+# ╠═c6bf8885-435d-4c3b-8c1f-ea6c32d607db
 # ╠═f7bb2230-22ba-4b40-8af2-dd7226e0a9b2
 # ╠═154fec09-f761-43aa-aaed-129d4d2f711e
 # ╠═4b783820-f72d-4ea8-bf8c-bbc2d556484c
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
